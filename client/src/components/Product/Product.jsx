4f8d9b7d0252380761944ebc486cfbb5dcbe31b5
Jimmy Skinnari <jimmyskinnari@hotmail.com>
2021-04-08 17:15:37 +0200

    more styling

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,14 +1,14 @@
 import React, { useContext } from 'react'
 import { CartContext } from '../../contexts/CartContextProvider'
 
-const Product = ({ item, id }) => {
+const Product = ({ item }) => {
 
     const { addToCart } = useContext(CartContext)
 
     return (
         <div className="product">
             <p className="productName">Product: {item.productName}</p>
-            <p className="productDescription">Description: about product here</p>
+            <p className="productDescription">Description: {item.description}</p>
             <p className="productPrice">Price: {item.price} kr</p>
             <button
                 className="firstPageBtn"
